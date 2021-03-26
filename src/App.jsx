8c6905ebb5ea26d5b00ed26f3000db7e87bb6274
Artem <64410376+Artiss90@users.noreply.github.com>
2021-03-26 02:44:30 +0200

    operands

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,53 @@ function App() {
   const [value, setValue] = useState(0);
   const [result, setResult] = useState(0);
   const [scope, setScope] = useState(0);
+  const [integer, setInteger] = useState(0);
+  // * флаг определяющий что было введено оператор или число
   const [operator, setOperator] = useState(false);
 
   useEffect(() => {
-    setResult(Number.parseFloat(value));
-  }, [value]);
+    if (operator) {
+      setScope(integer);
+      console.log(integer);
+      setInteger(Number.parseFloat(0));
+      console.log('next', integer);
+      return;
+    }
+    return;
+  }, [operator, result, integer]);
+
+  useEffect(() => {
+    setResult(Number.parseFloat(scope));
+  }, [scope]);
 
   const onClick = valueBtn => {
+    // ? присылаемое значение является числом
     if (typeof valueBtn === 'number') {
       console.log(typeof valueBtn);
       console.log(valueBtn.toString());
-      value !== 0
+      // *если значение 0 перезапишем, а не добавляем
+      Number(value) !== 0
         ? setValue(value + valueBtn.toString())
-        : setValue(valueBtn.toString());
+        : setValue(valueBtn);
+      Number(integer) !== 0
+        ? setInteger(integer + valueBtn.toString())
+        : setInteger(valueBtn);
+
+      setOperator(false);
+      return;
+    }
+    // ? присылаемое значение является оператором
+    if (valueBtn === '+') {
+      setOperator(true);
+      // (scope === )
+      setValue(value + valueBtn);
+      return;
+    }
+    // ? сброс значений
+    if (valueBtn === 'AC') {
+      setValue(0);
+      setResult(0);
+      return;
     }
     console.log('false', typeof valueBtn);
     return;
